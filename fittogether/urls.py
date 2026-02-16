090a3d7d5e21b003007032221614e755510fc6a9
ViVi09030 <vivianarango07@gmail.com>
2026-02-16 00:02:20 -0500

    Diseño de Profile implementado, aún faltan funcionalidades

--- fittogether/urls.py
+++ fittogether/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", include("social.urls")),
+    path("", include("users.urls")),
 ]
 
 if settings.DEBUG:
--- users/views.py
+++ users/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-
 """
 Responsabilidades:
 
@@ -8,3 +6,10 @@ Responsabilidades:
 - Ver perfil
 - Autenticación (login/logout)
 """
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+@login_required
+def profile_view(request):
+    return render(request, "users/profile.html")
