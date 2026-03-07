be65109befc8f2132f385aaeaffa6948c97651f3
jalvarez01 <juanjosealvarezag@gmail.com>
2026-03-07 08:55:59 -0500

    poder editar posts en 24 horas(aun esta esteticamente feo)

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+from datetime import timedelta
 
 
 class Post(models.Model):
@@ -8,5 +10,21 @@ class Post(models.Model):
     image = models.ImageField(upload_to="posts/", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def can_edit(self):
+        return timezone.now() <= self.created_at + timedelta(hours=24)
+
     def __str__(self):
         return f"{self.author.username} - {self.created_at:%Y-%m-%d %H:%M}"
+
+    def can_edit(self, user):
+        """
+        True if:
+        - user is authenticated
+        - user is the author
+        - post was created within last 24 hours
+        """
+        if not user.is_authenticated:
+            return False
+        if self.author != user:
+            return False
+        return timezone.now() <= self.created_at + timedelta(hours=24)
